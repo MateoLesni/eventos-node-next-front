ea133d019cae29081bb32102a4321c8f17e0f439
MateoLesni <mateolesniewicz@gmail.com>
2025-11-10 09:27:43 -0300

    normalizacion de nombres

--- app/carga/page.tsx
+++ app/carga/page.tsx
@@ -47,7 +47,7 @@ const API_BASE =
 
 const LUGARES = ["CoChinchina", "Kona", "Costa 7070", "Cruza Polo", "Milvidas", "We Enjoy"]
 const CANALES = ["Whatsapp NG", "Instagram", "Linkedin", "Web", "Mail Directo", "Referido"]
-const COMERCIALES = ["Pilar", "Tano", "Johanna", "Traianna", "Delfina"]
+const COMERCIALES = ["Pilar", "Tano", "Johanna Gatti", "Traianna Rosas", "Delfina Herrera Paz"]
 
 function CargaPageInner() {
   const search = useSearchParams()
